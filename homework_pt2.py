80bcd2aa8cf86c3389da909ecffaa8a86e56aba7
karen-campos1 <karencampos1994@yahoo.com>
2024-04-29 14:21:36 -0700

    first commit

---
+++ homework_pt2.py
@@ -0,0 +1,26 @@
+#3)Arithmetic Operations in Daily Life
+#Task1
+#give grocery items a price
+bread = 5
+peanut_butter = 6
+jelly = 8
+#add the total + say this is your total
+cart_total = bread + peanut_butter + jelly
+print("Your total is" , cart_total)
+
+
+#Task2
+#initial amount in savings
+initial_amt = 10000
+
+#fixed interest rate
+interest_rate = 7
+
+#calculate interest earned
+interest_earned = initial_amt * (interest_rate/ 100)
+
+#calculate total amt after 1 yr
+total_amt = initial_amt + interest_earned
+
+#print amt
+print("The total amount after a year is:" , total_amt)
