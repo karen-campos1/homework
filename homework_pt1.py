2427bffdf063d03219cc97daddcfac2ac191d80d
karen-campos1 <karencampos1994@yahoo.com>
2024-04-29 13:59:07 -0700

    first command

---
+++ homework_pt1.py
@@ -0,0 +1,25 @@
+#1)Showcase your dance moves!
+#Task 1 
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+#Task 2
+#ask how user feels today
+feeling = input("How do you feel today? options:happy or sad ")
+#check user response and print respond
+if feeling=="happy":
+    print("That's great to hear!")
+if feeling=="sad":
+    print("I hope your day gets better!")
+ 
+    
+#2)Python Naming Convention Practice
+PI_VALUE = 3.14 #all caps tells us this is a constant variable
+user_age = 25
+USER_LOCATION = "New York"
+max_limit = 1000
